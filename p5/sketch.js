0b5276340e68354a7ce8f9f28c061116b425c2a4
rrrrodrrrrigo <yectocsalev"gmail.com>
2019-10-01 11:43:57 -0400

    hola

---
+++ p5/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+    background(255, 170, 0);
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
